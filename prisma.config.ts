3abd9e463922d97972582f242824f8423285ddf6
Emanuel Lara <ealara199876@gmail.com>
2026-01-23 12:17:23 -0600

    fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from "prisma/config";
 
 const databaseUrl = process.env.DATABASE_URL;
-
 if (!databaseUrl) {
-  throw new Error(
-    "DATABASE_URL no está definida. Asegúrate de configurarla en Vercel"
-  );
+  throw new Error("DATABASE_URL no está definida");
 }
 
 export default defineConfig({
