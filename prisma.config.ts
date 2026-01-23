0c1fc035582d0fb060f002ba0d430716534f77e8
Emanuel Lara <ealara199876@gmail.com>
2026-01-23 12:02:15 -0600

    fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,21 @@
-import { config } from "dotenv";
-config(); // Loads .env automatically
-
 import { defineConfig } from "prisma/config";
 
 const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) throw new Error("DATABASE_URL not set");
+
+if (!databaseUrl) {
+  throw new Error(
+    "DATABASE_URL no está definida. Asegúrate de configurarla en Vercel"
+  );
+}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrations: { path: "prisma/migrations", seed: "tsx prisma/seed.ts" },
+  migrations: {
+    path: "prisma/migrations",
+    seed: "tsx prisma/seed.ts",
+  },
   engine: "classic",
-  datasource: { url: databaseUrl },
+  datasource: {
+    url: databaseUrl,
+  },
 });
